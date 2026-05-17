4f479a629511352d8c35dd4471495522011bdc5f
Roman-orn <0671635566@ukr.net>
2026-05-17 15:23:58 +0300

    Add Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,15 @@
 package app;
 
 public class Main {
+
+    static void main(String[] args) {
+
+        int[] data = new DataRepository().getData();
+        DataHandler dataHandler = new DataHandler();
+
+        MyThread myThread1 = new MyThread(data, dataHandler);
+        MyThread myThread2 = new MyThread(data, dataHandler);
+        myThread1.start();
+        myThread2.start();
+    }
 }
